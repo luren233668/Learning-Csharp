f53fc2a8ff9a580f189027d2866bcbb954f0d20b
luren233668 <luren233668@gmail.com>
2024-03-05 23:00:03 +0800

    完成第一次作业，并本地运行成功

---
+++ 第一次作业/Homework1/HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace HelloWorld;
+using System;
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("你好，我是侯旭阳");
+        Console.ReadLine();
+    }
+}
