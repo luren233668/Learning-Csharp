131313c3de5ab15a914585d1d674a3d588a3c265
luren233668 <luren233668@gmail.com>
2024-03-10 18:11:16 +0800

    初步完成作业1

--- 第二次作业/WindowsFormsHomework1/Form1.Designer.cs
+++ 第二次作业/WindowsFormsHomework1/Form1.Designer.cs
@@ -29,17 +29,101 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("仿宋", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(298, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "点击生成整数数组";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            this.button1.MouseHover += new System.EventHandler(this.button1_MouseHover);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("仿宋", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(207, 344);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(400, 80);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "数组为：\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("黑体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(25, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 70);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "最大值为：";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("黑体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(207, 175);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(150, 70);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "最小值为：";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("黑体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(401, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 70);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "平均值为：";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.Font = new System.Drawing.Font("黑体", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(604, 175);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(150, 70);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "和为：";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Homework1";
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+
+        private System.Windows.Forms.Label label1;
+
+        private System.Windows.Forms.Button button1;
 
         #endregion
     }
--- 第二次作业/WindowsFormsHomework1/Form1.cs
+++ 第二次作业/WindowsFormsHomework1/Form1.cs
@@ -15,6 +15,67 @@ namespace WindowsFormsHomework1
         public Form1()
         {
             InitializeComponent();
+            
+            SetButtonStyle(button1);
+            SetLabelStyle(label1);
+            SetLabelStyle(label2);
+            SetLabelStyle(label3);
+            SetLabelStyle(label4);
+            SetLabelStyle(label5);
+        }
+        
+        private void SetButtonStyle(Button button)
+        {
+            button.FlatStyle = FlatStyle.Flat;
+            button.ForeColor = Color.Transparent;
+            button.BackColor = Color.Transparent;
+            button.FlatAppearance.BorderSize = 0;
+            button.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            button.FlatAppearance.MouseDownBackColor = Color.Transparent;
+        }
+
+        private void button1_MouseHover(object sender, EventArgs eventArgs)
+        {
+            Button button = sender as Button;
+            button.FlatAppearance.BorderSize = 1;
+        }
+        
+        private void button1_MouseLeave(object sender, EventArgs eventArgs)
+        {
+            Button button = sender as Button;
+            button.FlatAppearance.BorderSize = 0;
+        }
+
+        private void SetLabelStyle(Label label)
+        {
+            label.ForeColor = Color.Transparent;
+            label.BackColor = Color.Transparent;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            var intArray = new int[6];
+            
+            for (int i = 0; i < 6; i++)
+            {
+                intArray[i] = random.Next(100);
+            }
+
+            label1.Text = "数组为：\n" + string.Join(" ", intArray);
+            
+            Array.Sort(intArray);
+            label2.Text = "最大值为： " + intArray[5].ToString();
+            label3.Text = "最小值为： " + intArray[0].ToString();
+
+            int sum = 0;
+            foreach (var i in intArray)
+            {
+                sum += i;
+            }
+
+            label4.Text = "平均值为： " + (sum / 6.0).ToString("f2");
+            label5.Text = "和为： " + sum;
         }
     }
 }
