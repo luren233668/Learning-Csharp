02b2caa6abe882866ef2d34cb72fb9c4429f6ed2
luren233668 <luren233668@gmail.com>
2024-03-11 20:53:56 +0800

    添加作业2窗口

---
+++ 第二次作业/WindowsFormsHomework1/Form2.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace WindowsFormsHomework1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+    }
+}
