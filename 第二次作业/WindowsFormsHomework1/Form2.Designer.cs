a8f2ade821403c8f809516690a65ab9052b81cd7
luren233668 <luren233668@gmail.com>
2024-03-11 22:57:46 +0800

    完成作业2

--- 第二次作业/WindowsFormsHomework1/Form2.Designer.cs
+++ 第二次作业/WindowsFormsHomework1/Form2.Designer.cs
@@ -33,11 +33,114 @@ namespace WindowsFormsHomework1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new Container();
-            this.AutoScaleMode = AutoScaleMode.Font;
-            this.ClientSize = new Size(800, 450);
-            this.Text = "Form2";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.Font = new System.Drawing.Font("仿宋", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(337, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(237, 66);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "请输入整数上限和下限";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("仿宋", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.textBox1.Location = new System.Drawing.Point(158, 120);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "下限：";
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_GotFocus);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_LostFocus);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("仿宋", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.textBox2.Location = new System.Drawing.Point(553, 120);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(180, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "上限：";
+            this.textBox2.Enter += new System.EventHandler(this.textBox2_GotFocus);
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_LostFocus);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button2.Font = new System.Drawing.Font("仿宋", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.ForeColor = System.Drawing.Color.Red;
+            this.button2.Location = new System.Drawing.Point(388, 192);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 32);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "点击计算";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.MouseLeave += new System.EventHandler(this.button2_MouseLeave);
+            this.button2.MouseHover += new System.EventHandler(this.button2_MouseHover);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.richTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.richTextBox1.Font = new System.Drawing.Font("仿宋", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.Red;
+            this.richTextBox1.Location = new System.Drawing.Point(221, 286);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.richTextBox1.Size = new System.Drawing.Size(480, 71);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(934, 525);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form2";
+            this.Text = "HomwWork2";
+            this.Resize += new System.EventHandler(this.Form2_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+
+        private System.Windows.Forms.Button button2;
+
+        private System.Windows.Forms.TextBox textBox2;
+
+        private System.Windows.Forms.TextBox textBox1;
+
+        private System.Windows.Forms.Label label1;
 
         #endregion
     }
--- 第二次作业/WindowsFormsHomework1/Form2.cs
+++ 第二次作业/WindowsFormsHomework1/Form2.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
 using System.Windows.Forms;
 
 namespace WindowsFormsHomework1
@@ -7,6 +11,282 @@ namespace WindowsFormsHomework1
         public Form2()
         {
             InitializeComponent();
+
+            //初始化控件样式
+            #region InitalizeStyle
+
+            SetLabelStyle(label1);
+            SetTextBoxStyle(textBox1);
+            SetTextBoxStyle(textBox2);
+            SetButtonStyle(button2);
+            
+            #endregion
+            
+            //初始化控件缩放
+            #region  InitializeResize
+ 
+            x = Width;
+            y = Height;
+            SetTag(this);
+ 
+            #endregion
         }
+
+        //设置控件样式
+        #region SetStyle
+
+        private void SetLabelStyle(Label label)
+        {
+            label.BackColor = Color.Transparent;
+        }
+
+        private void SetTextBoxStyle(TextBox textBox)
+        {
+            textBox.Size = new Size(180, 30);
+        }
+        
+        private void SetButtonStyle(Button button)
+        {
+            button.FlatStyle = FlatStyle.Flat;
+            button.BackColor = Color.Transparent;
+            button.FlatAppearance.BorderSize = 0;
+            button.FlatAppearance.MouseOverBackColor = Color.Transparent;
+            button.FlatAppearance.MouseDownBackColor = Color.Transparent;
+        }
+
+        private void button2_MouseHover(object sender, EventArgs eventArgs)
+        {
+            Button button = sender as Button;
+            button.FlatAppearance.BorderSize = 1;
+        }
+        
+        private void button2_MouseLeave(object sender, EventArgs eventArgs)
+        {
+            Button button = sender as Button;
+            button.FlatAppearance.BorderSize = 0;
+        }
+        
+        private void textBox1_GotFocus(object sender, EventArgs e)
+        {
+            // 清空文本框内容
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text == "下限：")
+            {
+                textBox.Text = "";
+            }
+        }
+
+        private void textBox1_LostFocus(object sender, EventArgs e)
+        {
+            // 如果文本框为空，恢复提示文本
+            TextBox textBox = (TextBox)sender;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "下限：";
+            }
+        }
+
+        private void textBox2_GotFocus(object sender, EventArgs e)
+        {
+            // 清空文本框内容
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text == "上限：")
+            {
+                textBox.Text = "";
+            }
+        }
+
+        private void textBox2_LostFocus(object sender, EventArgs e)
+        {
+            // 如果文本框为空，恢复提示文本
+            TextBox textBox = (TextBox)sender;
+            if (string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "上限：";
+            }
+        }
+
+        #endregion
+
+        //处理输入并输出
+
+        #region UseDataAndOut
+        
+        //合法性检验
+        private bool ValidateInput(string lowerLimitText, string upperLimitText, out int lowerLimit, out int upperLimit)
+        {
+            // 初始化输出参数
+            lowerLimit = 0;
+            upperLimit = 0;
+
+            // 检验是否为空
+            if (string.IsNullOrWhiteSpace(lowerLimitText) || string.IsNullOrWhiteSpace(upperLimitText))
+            {
+                MessageBox.Show("请输入有效的整数！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            // 检验是否为整数
+            if (!int.TryParse(lowerLimitText, out lowerLimit) || !int.TryParse(upperLimitText, out upperLimit))
+            {
+                MessageBox.Show("请输入有效的整数！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            // 检验是否为正数
+            if (lowerLimit <= 0 || upperLimit <= 0)
+            {
+                MessageBox.Show("请输入正整数！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            // 检验上下限关系
+            if (lowerLimit >= upperLimit)
+            {
+                MessageBox.Show("上限必须大于下限！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            // 合法性检验通过
+            return true;
+        }
+        
+        //生成素数列表
+        private List<int> GeneratePrimeNumbers(int lowerLimit, int upperLimit)
+        {
+            List<int> primes = new List<int>();
+
+            for (int i = lowerLimit; i <= upperLimit; i++)
+            {
+                if (IsPrime(i))
+                {
+                    primes.Add(i);
+                }
+            }
+
+            return primes;
+        }
+
+        //判断是否为素数
+        private bool IsPrime(int number)
+        {
+            if (number < 2)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+        
+        #endregion
+        
+        //计算符合条件的素数
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // 调用合法性检验函数
+            if (ValidateInput(textBox1.Text, textBox2.Text, out int lowerLimit, out int upperLimit))
+            {
+                // 合法性检验通过，可以继续处理
+                // 生成素数并显示在文本框中
+                var primeNumbers = GeneratePrimeNumbers(lowerLimit, upperLimit);
+                // 将素数按每行10个的方式显示在文本框中
+                DisplayPrimeNumbersInRows(primeNumbers, 10);
+            }
+        }
+        
+        private void DisplayPrimeNumbersInRows(List<int> primeNumbers, int numbersPerRow)
+        {
+            // 清空文本框
+            richTextBox1.Clear();
+
+            // 使用StringBuilder构建输出字符串
+            StringBuilder outputBuilder = new StringBuilder();
+
+            for (int i = 0; i < primeNumbers.Count; i++)
+            {
+                // 添加素数到StringBuilder
+                outputBuilder.Append(primeNumbers[i]);
+
+                // 添加分隔符（逗号和空格），除非是该行的最后一个数字
+                if (i % numbersPerRow != numbersPerRow - 1 && i < primeNumbers.Count - 1)
+                {
+                    outputBuilder.Append(", ");
+                }
+                else
+                {
+                    // 在达到每行限制或是最后一个数字时，添加换行符
+                    outputBuilder.AppendLine();
+                }
+            }
+
+            // 将StringBuilder中的内容显示在文本框中
+            richTextBox1.Text = outputBuilder.ToString();
+        }
+        
+        //设置窗口缩放
+        private void Form2_Resize(object sender, EventArgs e)
+        {
+            //重置窗口布局
+            ReWinformLayout();
+        }
+        
+        //实现控件大小随窗体大小等比例缩放
+        #region ResizeUI
+        
+        private float x; //定义当前窗体的宽度
+        private float y; //定义当前窗体的高度
+ 
+        //获取并保留所有控件的属性
+        private static void SetTag(Control cons)
+        {
+            foreach (Control con in cons.Controls)
+            {
+                con.Tag = con.Width + ";" + con.Height + ";" + con.Left + ";" + con.Top + ";" + con.Font.Size;
+                if (con.Controls.Count > 0)
+                {
+                    SetTag(con);
+                }
+            }
+        }
+ 
+        private static void SetControls(float newx, float newy, Control cons)
+        {
+            //遍历窗体中的控件，重新设置控件的值
+            foreach (Control con in cons.Controls)
+                //获取控件的Tag属性值，并分割后存储字符串数组
+                if (con.Tag != null)
+                {
+                    var mytag = con.Tag.ToString().Split(';');
+                    //根据窗体缩放的比例确定控件的值
+                    con.Width = Convert.ToInt32(Convert.ToSingle(mytag[0]) * newx); //宽度
+                    con.Height = Convert.ToInt32(Convert.ToSingle(mytag[1]) * newy); //高度
+                    con.Left = Convert.ToInt32(Convert.ToSingle(mytag[2]) * newx); //左边距
+                    con.Top = Convert.ToInt32(Convert.ToSingle(mytag[3]) * newy); //顶边距
+                    var currentSize = Convert.ToSingle(mytag[4]) * newy; //字体大小                   
+                    if (currentSize > 0)
+                    {
+                        con.Font = new Font(con.Font.Name, currentSize, con.Font.Style, con.Font.Unit);
+                    }
+                    con.Focus();
+                    if (con.Controls.Count > 0)
+                    {
+                        SetControls(newx, newy, con);
+                    }
+                }
+        }
+        
+        private void ReWinformLayout()
+        {
+            var newx = Width / x;
+            var newy = Height / y;
+            SetControls(newx, newy, this);
+ 
+        }
+        
+        #endregion
     }
 }
